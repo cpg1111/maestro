220e7ea2b6e0454d082361b7ed1e1eb011385ed4
Christian Grabowski <christiang11754@gmail.com>
2016-04-18 01:09:38 -0400

    config parser

---
+++ config/config.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+	"io/ioutil"
+
+	"github.com/BurntSushi/toml"
+)
+
+// Service is a struct of the service to build
+type Service struct {
+	Name      string
+	Tag       string
+	TagType   string
+	Path      string
+	BuildCMD  string
+	TestCMD   string
+	CheckCMD  string
+	CreateCMD string
+	UpdateCMD string
+	DependsOn string
+}
+
+type services struct {
+	Services []Service
+}
+
+// Project is the struct of the project to build
+type Project struct {
+	RepoURL        string
+	CloneCMD       string
+	AuthType       string
+	SSHPrivKeyPath string
+	SSHPubKeyPath  string
+	Username       string
+	Password       string
+	PromptForPWD   bool
+}
+
+// Config is a struct to parse the TOML config into
+type Config struct {
+	Project  Project
+	Services services
+}
+
+func readConfig(path string) ([]byte, error) {
+	return ioutil.ReadFile(path)
+}
+
+// LoadConfig reads the config and returns a Config struct
+func LoadConfig(path string) (Config, error) {
+	confData, readErr := readConfig(path)
+	if readErr != nil {
+		return nil, readErr
+	}
+	var conf Config
+	if _, pErr := toml.Decode(confData, &conf); pErr != nil {
+		return nil, pErr
+	}
+	return conf, nil
+}
