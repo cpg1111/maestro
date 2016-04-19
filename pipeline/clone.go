0e6e21eaad612f163fee50bdef92121f774d5122
Christian Grabowski <christiang11754@gmail.com>
2016-04-19 00:17:07 -0400

    pipeline service constructor

--- pipeline/clone.go
+++ pipeline/clone.go
@@ -5,14 +5,27 @@ import (
 	"os/exec"
 
 	"github.com/cpg1111/maestro/config"
+	"github.com/cpg1111/maestro/credentials"
 
-	git "github.com/libgit2/git2go"
+	git "gopkg.in/libgit2/git2go.v22"
 )
 
 type Service struct {
 	config.Service
-	Diff  bool
-	State string
+	Diff     bool
+	State    string
+	creds    credentials.RawCredentials
+	gitCreds git.Cred
+}
+
+func NewService(srv config.Service, creds credentials.RawCredentials) *Service {
+	return &Service{
+		srv,
+		Diff:     false,
+		State:    "Pending",
+		creds:    creds,
+		gitCreds: creds.ToGitCredentials(),
+	}
 }
 
 func (s *Service) ShouldRun() (bool, error) {
