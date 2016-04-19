57d795ff1f53d60f512e4ac7bf19dd580d0ff690
Christian Grabowski <christiang11754@gmail.com>
2016-04-19 00:36:54 -0400

    project constructor

--- pipeline/clone.go
+++ pipeline/clone.go
@@ -28,17 +28,40 @@ func NewService(srv config.Service, creds credentials.RawCredentials) *Service {
 	}
 }
 
-func (s *Service) ShouldRun() (bool, error) {
-
-}
-
-func (s *Service) Build() error {
+func (s *Service) ShouldRunPipeLine() (bool, error) {
 
 }
 
 type Project struct {
 	config.Project
-	State    string
-	ABSPath  string
-	Services map[string]Service
+	State     string
+	ABSPath   string
+	Services  map[string]*Service
+	creds     credentials.RawCredentials
+	gitCreds  git.Cred
+	clonePath string
+}
+
+func New(conf config.Config, creds credentials.RawCredentials, clonePath string) *Project {
+	newServices := make(map[string]*Service)
+	for i := range conf.Services {
+		newServices[conf.Services[i].Name] = NewService(conf.Services[i], creds)
+	}
+	cwd, cwdErr := os.Getwd()
+	if cwdErr != nil {
+		panic(cwdErr)
+	}
+	return &Project{
+		conf.Project,
+		State:     "Pending",
+		ABSPath:   fmt.Sprintf("%s%s", cwd, clonePath),
+		Services:  newServices,
+		creds:     creds,
+		gitCreds:  creds.ToGitCredentials,
+		clonePath: clonePath,
+	}
+}
+
+func (p *Project) Clone(opts *git.CloneOptions) (git.Repository, error) {
+	return git.Clone(p.RepoURL, p.clonePath, opts)
 }
