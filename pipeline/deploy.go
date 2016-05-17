277b408396717a144cd2111b746f2caf626e3d8b
Christian Grabowski <christiang11754@gmail.com>
2016-05-17 00:03:49 -0400

    concurrent pipelines

--- pipeline/deploy.go
+++ pipeline/deploy.go
@@ -10,7 +10,33 @@ func check(srv *Service) error {
 		return checkErr
 	}
 	if check {
-		log.Println(check)
+		log.Println("Found service, updating now")
+		err := update(srv)
+		if err != nil {
+			return err
+		}
+	} else {
+		log.Println("Creating new service")
+		err := create(srv)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func create(srv *Service) error {
+	err := srv.execCreate()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func update(srv *Service) error {
+	err := srv.execUpdate()
+	if err != nil {
+		return err
 	}
 	return nil
 }
