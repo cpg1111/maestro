fd1333ffdb9997a4af3fbac9a6e3c51a61928af1
Christian Grabowski <christiang11754@gmail.com>
2016-05-27 00:19:50 -0400

    add cleanup phase

--- cleanUp/cleanUp.go
+++ cleanUp/cleanUp.go
@@ -1,20 +1,86 @@
 package cleanUp
 
 import (
+	"io/ioutil"
 	"os"
 
 	"github.com/cpg1111/maestro/config"
+	"github.com/cpg1111/maestro/util"
 )
+
+func handleCMDs(cmds []string) error {
+	pwd, pwdErr := os.Getwd()
+	if pwdErr != nil {
+		return pwdErr
+	}
+	if len(cmds) > 0 {
+		for i := range cmds {
+			cmd, cmdErr := util.FormatCommand(cmds[i], pwd)
+			if cmdErr != nil {
+				return cmdErr
+			}
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			err := cmd.Run()
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func saveArtifacts(artifacts []config.Artifact) error {
+	artifactsLen := len(artifacts)
+	if artifactsLen > 0 {
+		finished := make(chan bool)
+		err := make(chan error)
+		for i := range artifacts {
+			go func() {
+				buildFile, readErr := ioutil.ReadFile(artifacts[i].RuntimeFilePath)
+				if readErr != nil {
+					err <- readErr
+					finished <- false
+				} else {
+					writeErr := ioutil.WriteFile(artifacts[i].SaveFilePath, buildFile, 0644)
+					if writeErr != nil {
+						err <- writeErr
+						finished <- false
+					} else {
+						finished <- true
+					}
+				}
+			}()
+		}
+		total := 0
+		for {
+			select {
+			case done := <-finished:
+				if done {
+					total++
+				}
+				if total == artifactsLen {
+					return nil
+				}
+			case errMsg := <-err:
+				if errMsg != nil {
+					return errMsg
+				}
+			}
+		}
+	}
+	return nil
+}
 
 // Run runs the clean tasks
 func Run(conf *config.CleanUp, clonePath *string) error {
-	if len(conf.AdditionalCMDs) > 0 {
-		// TODO run AdditionalCMDs
-		return nil
+	cmdErr := handleCMDs(conf.AdditionalCMDs)
+	if cmdErr != nil {
+		return cmdErr
 	}
-	if len(conf.Artifacts) > 0 {
-		// TODO save artifacts
-		return nil
+	artifactErr := saveArtifacts(conf.Artifacts)
+	if artifactErr != nil {
+		return artifactErr
 	}
 	if conf.InDaemon {
 		// TODO send status to daemon
